9fa077d534e3658df4d282a47a8df0c592434cbb
George <George@DESKTOP-SLIQCDH>
2023-03-08 20:15:46 +0000

    Add project files.

---
+++ GelbooruDL/Classes/Result.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using GelbooruDL;
+
+namespace GelbooruDL.Classes
+{
+    public class Result
+    {
+        public string pageURL = "";
+        public List<string> tags = new List<string>();
+        public string thumImage = "";
+        public string imageURL = "";
+
+        public Result(string pageURL, List<string> tags, string thumImage)
+        {
+            this.pageURL = pageURL;
+            this.tags = tags;
+            this.thumImage = thumImage;
+        }
+
+        public string GetTagString()
+        {
+            return string.Join(", ", tags);
+        }
+
+        public string GetTagSummary()
+        {
+            int k = tags.Count;
+            if (k > 10) { k = 10; }
+
+            List<string> toReturn = new List<string>();
+            for (int i = 0; i < k; i++)
+            {
+                toReturn.Add(tags[i]);
+            }
+
+
+            return string.Join(", ", toReturn);
+        }
+
+        public string GetImageExtension()
+        {
+            try
+            {
+                return imageURL.Split(".")[3];
+            } catch
+            {
+                Program.printColour("Something unexpected happened when trying to get the image extension", ConsoleColor.Red);
+                Program.printColour("Trying to carry on", ConsoleColor.Red);
+                return "jpg";
+            }
+            
+        }
+
+        public bool downloadImageFromPage(bool fullSize = true)
+        {
+            WebClient client = new WebClient();
+            string html = client.DownloadString(pageURL);
+
+            string snipStart = string.Empty;
+            int snipIndex = -1;
+
+            snipIndex = html.IndexOf("<a href=\"https://img3.gelbooru.com/images/") + 9;
+            html = html.Substring(snipIndex);
+
+            snipIndex = html.IndexOf("\"");
+            html = html.Substring(0, snipIndex);
+            imageURL = html;
+
+            string filename = GetTagSummary();
+            string fileExtension = GetImageExtension();
+            try
+            {
+                client.DownloadFile(imageURL, $"Downloads\\{filename}.{fileExtension}");
+                string captionFilePath = $"Downloads\\{filename}.txt";
+                File.CreateText(captionFilePath).Dispose();
+                File.WriteAllText(captionFilePath, GetTagString());
+            }
+            catch (Exception ex)
+            {
+                Program.printColour($"Failed to download image: {imageURL}", ConsoleColor.Red);
+                Program.printColour($"{ex.Message} ({ex.GetType()})", ConsoleColor.Red);
+                if (ex.InnerException.Message != null)
+                {
+                    Program.printColour(ex.InnerException.Message, ConsoleColor.Red);
+                }
+                return false;
+            }
+
+
+            Program.printColour($"Saved image with tags '{GetTagSummary()}...'");
+            return true;
+        }
+    }
+}
---
+++ GelbooruDL/Program.cs
@@ -0,0 +1,101 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Net;
+using GelbooruDL.Classes;
+using System.Drawing;
+
+namespace GelbooruDL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            if (!Directory.Exists("Downloads"))
+            {
+                Directory.CreateDirectory("Downloads");
+            }
+
+            if (args.Length > 0) //if exe is called with arguments
+            {
+                string searchArgument = string.Join(" ", args);
+                search(searchArgument);
+                return;
+            }
+        start:
+            Console.Write("Enter tags or url: ");
+            string input = Console.ReadLine().Trim();
+            if (input.Contains("danbooru") || input.Contains(".exe") || input.Contains(".js")) { printColour("That won't work!", ConsoleColor.Red); return; }
+
+            search(input);
+            printColour("Finished job", ConsoleColor.Blue);
+            Console.WriteLine();
+            goto start;
+        }
+
+        static void search(string searchString)
+        {
+            string url = !searchString.StartsWith("https://gelbooru.com/") ? $"https://gelbooru.com/index.php?page=post&s=list&tags={searchString}" : searchString;
+            printColour($"Searching images from {url}", ConsoleColor.White);
+
+            WebClient client = new WebClient();
+            string html = client.DownloadString(url);
+
+            List<Result> results = getResults(html);
+            printColour($"Found {results.Count} images", ConsoleColor.White);
+
+            printColour($"Downloading images...", ConsoleColor.White);
+            downloadSearchResults(results);
+
+            Console.WriteLine("Done!");
+        }
+
+        static void downloadSearchResults(List<Result> results)
+        {
+            foreach (Result result in results)
+            {
+                result.downloadImageFromPage();
+                Thread.Sleep(1000);
+            }
+        }
+
+        static List<Result> getResults(string html)
+        {
+            var htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(html);
+
+            var searchResults = htmlDoc.DocumentNode.SelectNodes(@"/html/body/div[1]/main/div[7]").ToList()[0];
+
+            List<Result> results = new List<Result>();
+            foreach (var result in searchResults.ChildNodes)
+            {
+                if (result.Name != "article") { continue; }
+                var resultChildren = result.ChildNodes.ToList()[1].ChildNodes.ToList()[1];
+                /*
+                resultChildren attributes:
+                    * src (thumnail image)
+                    * title (tags)
+                    * alt (alt string containing tags)
+                    * class (contains nothing)
+                */
+
+                string sauce = resultChildren.Attributes["src"].Value; //mhmm saucy
+                List<string> tagsList = resultChildren.Attributes["title"].Value.Split("  ")[0].Trim().Split(" ").ToList();
+                string tags = string.Join(", ", tagsList);
+                string pageURL = result.ChildNodes.ToList()[1].Attributes["href"].DeEntitizeValue;
+
+                Result newResult = new Result(pageURL, tagsList, sauce);
+                results.Add(newResult);
+            }
+
+            return results;
+        }
+
+        public static void printColour(string message, ConsoleColor colour = ConsoleColor.Green)
+        {
+            Console.ForegroundColor = colour;
+            Console.WriteLine(message);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
